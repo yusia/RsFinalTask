9adaa8ea9731a99d78a4115d03f0b68da5ab5bf1
julia <yusiagerachka@gmail.com>
2023-02-26 22:58:50 +0200

    fix: build

--- src/contants.ts
+++ src/contants.ts
@@ -1,9 +1,4 @@
-// export const Constants = {
-//   serverUrl: 'https://clonefinalback.onrender.com/',
-//   DefaultTimer: 90,
-// };
-
 export const Constants = {
-  serverUrl: 'http://localhost:4444/',
-  DefaultTimer: 90
+  serverUrl: 'https://clonefinalback.onrender.com/',
+  DefaultTimer: 90,
 };
